cdf09732fe638543aed70b180b494ff52b30ed5d
Aitor Ameztegui <ameztegui@gmail.com>
2020-07-16 23:10:55 +0200

    added data and documentation

---
+++ R/neighbors.R
@@ -0,0 +1,16 @@
+#' Forest inventory data in Pyrenean forests
+#'
+#' @source Aitor Ameztegui
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{plot}{Plot identifier}
+#'  \item{sps}{Tree species, with 3 possible values: ABAL, PINI or PISY}
+#'  \item{dbh}{Tree diameter(cm).}
+#'  \item{x}{x coordinates of the tree within the plot, as measured from the plot center (m).}
+#'  \item{y}{y coordinates of the tree within the plot, as measured from the plot center (m).}
+#' }
+#' @examples
+#' \dontrun{
+#'  tree_data
+#' }
+"tree_data"
