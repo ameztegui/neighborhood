5c9581cd844462a0d5fc8e7b83ec5cfb846b33ce
Aitor Ameztegui <ameztegui@gmail.com>
2020-07-18 20:54:41 +0200

    new optional arguments: coords, suffixes, max_dist

--- R/get_neighbors.R
+++ R/get_neighbors.R
@@ -9,6 +9,11 @@
 #' @param plot_ID **optional**. Variable that identifies the plots, or experimental units, within which
 #' the neighbors will be searched. If this variable exists, a tree "a" can only be a neighbor of
 #' another tree "b", if it is fulfilled that plot(a) == plot(b).
+#' @param coords **optional** Character vector containing the names of the columns in `df`containing the x and y coordinates of the trees.
+#' By default `coords = c("x", "y")`, i.e. it assumes taht the columns are called "x" and "y"
+#' @param suffixes **optional** character vector containing the suffixes that will be added to the variables in `df`to dientify target trees and neighbours.
+#' By default it takes the value `suffixes = c("_target", "_neighbor")`, but can take any other value defined by user.
+#' @param max_dist numeric Maximum distance to search for neighbours of target trees. It takes value 10000 by default.
 #'
 #' @return This function identifies the neighbors of each potential "target" tree and creates a data frame
 #' that contains a row for each neighbor of each tree in the original data frame. Variables characterizing
@@ -27,8 +32,25 @@
 #'
 #' neighbors <- get_neighbors(tree_data, plot)
 #'
+#' # If not plots are to be considered
+#' all_neighbors <- get_neighbors(tree_data)
 #'
-get_neighbors <- function (df, plot_ID,... ) {
+#' # specify suffix for target and neighbors, and maximum distance
+#' neighbors2 <- get_neighbors(tree_data, plot, suffixes = c("cible", "voisin"), max_dist = 10)
+
+#'
+#'
+get_neighbors <- function (df, plot_ID, coords, suffixes, max_dist = 10000 ) {
+
+    if(missing(coords)) coords <- c("x", "y")
+    if(missing(suffixes)) suffixes <- c("target", "neighbor")
+
+    x_target <- paste0(coords[1], suffixes[1])
+    y_target <- paste0(coords[2], suffixes[1])
+    x_neighbor <- paste0(coords[1], suffixes[2])
+    y_neighbor <- paste0(coords[2], suffixes[2])
+
+    #####
 
     if(missing(plot_ID)) {
         targets <- df %>%
@@ -36,10 +58,8 @@ get_neighbors <- function (df, plot_ID,... ) {
             mutate(ID = sequence(nrow(df)))
 
         neighbors <- full_join(targets, targets, by ="zzz",
-                               suffix = c("_target", "_neighbor")) %>%
-            select(-zzz) %>%
-            mutate(dist=sqrt((x_neighbor - x_target)^2 +
-                                 (y_neighbor - y_target)^2) )
+                               suffix = c(suffixes))
+
     } else {
 
         targets <- df %>%
@@ -51,9 +71,12 @@ get_neighbors <- function (df, plot_ID,... ) {
 
         neighbors <- full_join(targets, targets, by = c("zzz",
                                                         quo_name(enquo(plot_ID))),
-                               suffix = c("_target", "_neighbor")) %>%
-            select(-zzz) %>%
-            mutate(dist=sqrt((x_neighbor - x_target)^2 +
-                                 (y_neighbor - y_target)^2) )
+                               suffix = suffixes)
     }
+
+    neighbors %>%
+        select(-zzz) %>%
+        mutate(dist = sqrt((!!sym(x_neighbor) - !!sym(x_target))^2 +
+                               (!!sym(y_neighbor) - !!sym(y_target))^2)) %>%
+        filter(dist <= max_dist)
 }
