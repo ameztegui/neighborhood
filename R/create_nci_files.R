3147d1f4cca755c8b42d101b515d40f0f9f1b398
ameztegui <ameztegui@gmail.com>
2020-12-17 00:44:21 +0100

    updated filters to creation of nci files

--- R/create_nci_files.R
+++ R/create_nci_files.R
@@ -16,14 +16,21 @@
 #' @param y variable in `df` that contains the Y coordinate of the trees.  Coordinates can be referred to any reference point (typically
 #' they will refer to either one of the plot corners or the plot center), but all of them must be referred to the same point to allow calculating distances
 #' between trees.
-#'
-#' @param plot_ID optional. Variable that identifies the plots, or experimental units, within which
-#' the neighbors will be considered. This information is used to split the calculations per plot, and the results
-#' are merged back into a single data frame. A tree `b` will not be considered as a neighbor of tree `a` unless they belong to the same
-#' `plot_ID`. If this argument is missing, all trees in `df` will be considered both targets and neighbors.
-#'
+#'#'
 #' @param self_del If `TRUE`, a tree is not included as a neighbor of itself. If `FALSE` the attribute if the target tree will be also
 #' included in the output as a neighbor of itself.
+#' @param ... optional. Character or numeric variable that identifies the plots, or experimental units, within which
+#' the neighbors will be considered. This information is used to split the calculations per plot, and the results
+#' are merged back into a single data frame. A tree `b` will not be considered as a neighbor of tree `a` unless they belong to the same
+#' plot. If this argument is missing, all trees in `df` will be considered both targets and neighbors.
+
+#
+#' @param min.target.dbh numeric The minimum dbh for a tree to be considered as a target. The function will only return neighbors of
+#' those target trees with dbh >= min.target.dbh. If not specified, all trees in `df` will be considered targets
+#' @param min.neighbor.dbh numeric The minimum dbh for a tree to be considered as a neighbor. All those neighbors of a given target with dbh
+#' above this threshold will be ignored. If not specified, all the neighbors of a given target will be considered
+#' @param max.neighbor.radius numeric maximum radius to search for neighbors. Only those neighbors located closer than this distance
+#' from a target tree will be considered their neighbors.
 #'
 #' @return a list containing three data frames with the values of dbh, species and distance to target tree for each neighbor tree.
 #' Each data frame will contain as many rows as target trees, and as many columns as the maximum number of neighbors per tree.Missing values are coded as `NA`.
@@ -34,20 +41,58 @@
 #'
 #' files <- create_nci_files(tree_data, dbh = dbh, sp = sps,x = x, y = y, plot)
 #'
-create_nci_files <- function (df, dbh, sp, x, y, plot_ID, self_del = T) {
+#'
+#' # add some restrictions on target size and searching distance
+#' files2 <- create_nci_files(tree_data, dbh, sp, x, y, self_del = T, plot,
+#' min.target.dbh = 100, min.neighbor.dbh = 20, max.neighbor.radius = 10)
+#'
+#'
+create_nci_files <- function (df, dbh, sp, x, y, self_del = T,...,
+                              min.target.dbh = NULL, min.neighbor.dbh = NULL,
+                              max.neighbor.radius = NULL) {
 
-    dbhs <- get_neighbor_variables(df, {{plot_ID}}, {{dbh}}, self_del = {{self_del}})
-    sps <- get_neighbor_variables(df, {{plot_ID}}, {{sp}}, self_del = {{self_del}})
-    x_matrix <- get_neighbor_variables(df, {{plot_ID}}, {{x}}, self_del = {{self_del}})
-    y_matrix <- get_neighbor_variables(df, {{plot_ID}},  {{y}}, self_del = {{self_del}})
+    targets <- df
+    dbhs <- get_neighbor_variables(df, {{dbh}}, self_del, ...)
+    sps <- get_neighbor_variables(df, {{sp}}, self_del, ...)
+    x_matrix <- get_neighbor_variables(df, {{x}}, self_del , ...)
+    y_matrix <- get_neighbor_variables(df,  {{y}}, self_del, ...)
 
-    xdist_matrix <- x_matrix - df %>% arrange({{plot_ID}}) %>% pull({{x}})
-    ydist_matrix <- y_matrix - df %>% arrange({{plot_ID}}) %>% pull({{y}})
+    xdist_matrix <- x_matrix - df %>% arrange(...) %>% pull({{x}})
+    ydist_matrix <- y_matrix - df %>% arrange(...) %>% pull({{y}})
     distances <- sqrt(xdist_matrix^2 + ydist_matrix^2)
 
 
-    list(dbhs = dbhs, species = sps, distances = distances)
-}
+    #####################################################
+    #  Set up some limits for the analysis by creating vectors that contain TRUE or FALSE for whether each observation
+    # meets all of your criteria
 
+    # Minimum target dbh
+    if(!is.null(min.target.dbh)) {
+        using.targets <- which (targets$dbh >= min.target.dbh)
+
+        # Eliminate rows from the neighbor matrices for targets we're not using
+        targets <- df[using.targets,]   # this creates a subset of targets for which "using.targets" is true
+        dbhs <- dbhs[using.targets,]
+        sps <- sps[using.targets,]
+        distances <- distances[using.targets,]
+    }
+
+    if(!is.null(max.neighbor.radius)) {
+        # Get rid of neighbors beyond the max radius
+        distances[distances >= max.neighbor.radius] <- NA
+    }
+    if(!is.null(min.neighbor.dbh)) {
+        distances[dbhs <= min.neighbor.dbh] <- NA
+    }
+    distances[distances == 0 ] <- NA
+    dbhs[is.na(distances)]  <- NA
+    sps[is.na(dbhs)] <- NA
+    distances[is.na(dbhs)] <- NA
+
+    list(targets = targets,
+         dbhs = dbhs %>% select_if(~!all(is.na(.))),
+         sps = sps %>% select_if(~!all(is.na(.))),
+         distances = distances %>% select_if(~!all(is.na(.))))
+}
 
 
--- R/get_neighbor_variables.R
+++ R/get_neighbor_variables.R
@@ -6,14 +6,14 @@
 #' @description function to obtain the attributes of all the neighbors of a given set of target trees
 #'
 #' @param df the data frame containing information about the target trees.
-#' @param plot_ID optional. Variable that identifies the plots, or experimental units, within which
-#' the neighbors will be considered. This information is used to split the calculations per plot, and the results
-#' are merged back into a single data frame. A tree `b` will not be considered as a neighbor of tree `a` unless they belong to the same
-#' `plot_ID`. If this argument is missing, all trees in `df` will be considered both targets and neighbors.
 #' @param var The variable that we want to extract from the neighbors. It can be either a numeric, logical or
 #' character variable.
 #' @param self_del If `TRUE`, a tree is not included as a neighbor of itself. If `FALSE` the attribute if the target tree will be also
 #' included in the output as a neighbor of itself.
+#' @param ... optional. Character or numeric variable that identifies the plots, or experimental units, within which
+#' the neighbors will be considered. This information is used to split the calculations per plot, and the results
+#' are merged back into a single data frame. A tree `b` will not be considered as a neighbor of tree `a` unless they belong to the same
+#' plot. If this argument is missing, all trees in `df` will be considered both targets and neighbors.
 #' @return a data frame containing as many rows as target trees, and as many columns as the maximum number of neighbors per tree.
 #' Cells contain values of the variable "var" for each neighbor tree, and missing values are coded as `NA`.
 #'
@@ -21,24 +21,32 @@
 #'
 #' data(tree_data)
 #'
-#' # Get dbhs of all the neighbirs
-#' dbhs <-get_neighbor_variables(tree_data, plot, dbh, self_del = T)
+#' # Get dbhs of all the neighbors, as if they were all in the same plot
+#' dbhs <-get_neighbor_variables(tree_data, var = dbh, self_del = F)
 #'
 #' # Get species of all the neighbors (incuding target tree as its own neighbor)
-#' species <-get_neighbor_variables(tree_data, plot, sps, self_del = F)
+#' species <-get_neighbor_variables(tree_data, var = sps, plot, self_del = T)
 #'
-get_neighbor_variables <- function (df, plot_ID, var, self_del = T) {
+
+
+get_neighbor_variables <- function (df,  var, self_del = T, ... ) {
 
     library(tidyverse)
-    df_split <- group_split(df, {{plot_ID}})
+
+    df_split <- group_split(df, ...)
+
 
     create_file <- function (df_split, var) {
         var2 = pull(df_split, {{var}})
         file <-data.frame(matrix(rep(var2, length(var2)), ncol = length(var2), byrow = T ))
 
-        if(self_del== T) file[row(file) == col(file)] <- NA
+        if(self_del == T) file[row(file) == col(file)] <- NA
         file
     }
 
     map_df(df_split, create_file, {{var}})
 }
+
+
+
+
